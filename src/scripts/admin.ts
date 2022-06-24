cbbee21b12036d4b721a98ee24e7c5102e580622
jaimytaco <jaimy.taco@gmail.com>
2022-06-24 18:01:34 -0500

    feat: create-service-dialog

--- src/scripts/admin.ts
+++ src/scripts/admin.ts
@@ -20,23 +20,23 @@ const handleDialog = (id, action) => {
 }
 
 const initDialog = (id) => {
-    const closeBtns = [...document.querySelectorAll('[data-close-dialog]')]
+    const closeBtns = [...document.querySelectorAll('[data-close-dialog_btn]')]
     closeBtns.forEach((btn) => btn.onclick = () => {
-        const id = btn.getAttribute('data-close-dialog')
+        const id = btn.getAttribute('data-close-dialog_btn')
         handleDialog(id, 'remove')
     })
 
-    const stepBackBtns = [...document.querySelectorAll(['[data-previous-step]'])]
+    const stepBackBtns = [...document.querySelectorAll(['[data-previous-step_btn]'])]
     stepBackBtns.forEach((btn) => btn.onclick = () => {
         const currentFormId = btn.getAttribute('data-current-step')
         if (!currentFormId) throw 'data-current-step not found'
         const currentForm = document.getElementById(currentFormId)
         if (!currentForm) throw 'data-current-step form not found'
 
-        const previousFormId = btn.getAttribute('data-previous-step')
-        if (!previousFormId) throw 'data-previous-step not found'
+        const previousFormId = btn.getAttribute('data-previous-step_btn')
+        if (!previousFormId) throw 'data-previous-step_btn not found'
         const previousForm = document.getElementById(previousFormId)
-        if (!previousForm) throw 'data-previous-step form not found'
+        if (!previousForm) throw 'data-previous-step_btn form not found'
 
         currentForm.classList.remove('active')
         previousForm.classList.add('active')
@@ -44,21 +44,21 @@ const initDialog = (id) => {
 }
 
 const registerOrder = () => {
-    const btn = document.getElementById('register-order_btn')
-    if (!btn) throw 'register-order_btn not found'
-    const formStep1 = document.getElementById('register-order-step-1_form')
-    if (!formStep1) throw 'register-order-step-1_form not found'
-    const formStep2 = document.getElementById('register-order-step-2_form')
-    if (!formStep2) throw 'register-order-step-2_form not found'
-    const formStep3 = document.getElementById('register-order-step-3_form')
-    if (!formStep3) throw 'register-order-step-3_form not found'
-    const formStep4 = document.getElementById('register-order-step-4_form')
-    if (!formStep4) throw 'register-order-step-4_form not found'
-    const formStep5 = document.getElementById('register-order-step-5_form')
-    if (!formStep5) throw 'register-order-step-5_form not found'
+    const btn = document.getElementById('create-order_btn')
+    if (!btn) throw 'create-order_btn not found'
+    const formStep1 = document.getElementById('create-order-step-1_form')
+    if (!formStep1) throw 'create-order-step-1_form not found'
+    const formStep2 = document.getElementById('create-order-step-2_form')
+    if (!formStep2) throw 'create-order-step-2_form not found'
+    const formStep3 = document.getElementById('create-order-step-3_form')
+    if (!formStep3) throw 'create-order-step-3_form not found'
+    const formStep4 = document.getElementById('create-order-step-4_form')
+    if (!formStep4) throw 'create-order-step-4_form not found'
+    const formStep5 = document.getElementById('create-order-confirmation-step-5_form')
+    if (!formStep5) throw 'create-order-confirmation-step-5_form not found'
 
     btn.onclick = () => {
-        handleDialog('register-order_dialog', 'add')
+        handleDialog('create-order_dialog', 'add')
         formStep1.classList.add('active')
     }
 
@@ -100,17 +100,17 @@ const registerOrder = () => {
 }
 
 const initTableExtra = () => {
-    const showTableBtns = [...document.querySelectorAll('[data-show-table-extra]')]
+    const showTableBtns = [...document.querySelectorAll('[data-show-table-extra_id]')]
     showTableBtns.forEach((btn) => {
         btn.onclick = () => {
-            const id = btn.getAttribute('data-show-table-extra')
-            if (!id) throw 'table-extra id not found'
+            const id = btn.getAttribute('data-show-table-extra_id')
+            if (!id) throw 'data-show-table-extra_id not found'
             const extra = document.getElementById(id)
-            if (!extra) throw 'table-extra not found'
+            if (!extra) throw 'data-show-table-extra_id item not found'
 
-            const btns = [...document.querySelectorAll(`[data-show-table-extra="${id}"]`)]
+            const btns = [...document.querySelectorAll(`[data-show-table-extra_id="${id}"]`)]
             btns.forEach((b) => {
-                const content = b.getAttribute(`data-show-table-extra-${extra.matches('.active') ? 'open' : 'close'}`)
+                const content = b.getAttribute(`data-show-table-extra_id-${extra.matches('.active') ? 'open' : 'close'}`)
                 if (content) b.textContent = content
                 b.classList.toggle('active')
             })
@@ -125,38 +125,38 @@ const initTable = () => {
 }
 
 const initCard8 = () => {
-    const btns = [...document.querySelectorAll('[data-c-8]')]
+    const btns = [...document.querySelectorAll('[data-c-8_btn]')]
     btns.forEach((btn) => {
         btn.onclick = () => btn.parentNode.classList.toggle('active')
     })
 }
 
-const initNewSaleDialog = (dialogId, currentSales) => {
+const configCreateSaleDialog = (dialogId, currentSales) => {
     const dialog = document.querySelector(`#${dialogId}`)
-    if (!dialog) throw `${dualogId} not found`
+    if (!dialog) throw `${dialogId} not found`
 
     const btn = document.getElementById('cart_btn')
     if (!btn) throw 'cart_btn not found'
 
     btn.onclick = () => {
         !currentSales.length ?
-            () => {
+            (() => {
                 handleDialog(dialogId, 'add')
                 formStep1.classList.add('active')
-            } : null
+            })() : null
     }
 
-    const formStep1 = dialog.querySelector('#new-sale-step-1_form')
-    if (!formStep1) throw 'new-sale-step-1_form not found'
-    const formStep2 = dialog.querySelector('#new-sale-step-2_form')
-    if (!formStep2) throw 'new-sale-step-2_form not found'
-    const formStep3 = dialog.querySelector('#new-sale-step-3_form')
-    if (!formStep3) throw 'new-sale-step-3_form not found'
+    const formStep1 = dialog.querySelector('#create-sale-step-1_form')
+    if (!formStep1) throw 'create-sale-step-1_form not found'
+    const formStep2 = dialog.querySelector('#create-sale-step-2_form')
+    if (!formStep2) throw 'create-sale-step-2_form not found'
+    const formStep3 = dialog.querySelector('#create-sale-confirmation-step-3_form')
+    if (!formStep3) throw 'create-sale-confirmation-step-3_form not found'
     
     formStep1.onsubmit = (e) => {
         e.preventDefault()
 
-        // TODO: new-sale-step-1_form logic
+        // TODO: create-sale-step-1_form logic
 
         formStep1.classList.remove('active')
         formStep2.classList.add('active')
@@ -165,7 +165,7 @@ const initNewSaleDialog = (dialogId, currentSales) => {
     formStep2.onsubmit = (e) => {
         e.preventDefault()
 
-        // TODO: new-sale-step-2_form logic
+        // TODO: create-sale-step-2_form logic
 
         formStep2.classList.remove('active')
         formStep3.classList.add('active')
@@ -179,39 +179,37 @@ const initNewSaleDialog = (dialogId, currentSales) => {
 
     btnAddOrder.onclick = () => {
         formStep3.classList.remove('active')
+        handleDialog(dialogId, 'remove')
+        
         // TODO: open add-order dialog
+        
+        configCreateServiceDialog('create-service_dialog')
+        fireCreateServiceDialog('create-service_dialog')
     }
 
     btnRetry.onclick = () => {
         formStep1.reset()
         formStep2.reset()
+
+        formStep3.classList.remove('active')
         formStep1.classList.add('active')
     }
 }
 
-const initFieldsetList = () => {
-    const inputs = [...document.querySelectorAll('[data-fs-list-input]')]
-    inputs.forEach((input) => {
-        input.oninput = (e) => {
-            console.log('input =', input.value)
-        }
-    }) 
-}
-
-const initCard11 = (cards) => {
+const initCart = (cards) => {
     cards.forEach((card) => {
-        const btnAdd = card.querySelector('[data-add-item]')
-        if (!btnAdd) throw 'card-11 btn data-add-item not found'
+        const btnAdd = card.querySelector('[data-addtocart-item_btn]')
+        if (!btnAdd) throw 'data-addtocart-item_btn not found'
 
-        const btnRemove = card.querySelector('[data-remove-item]')
-        if (!btnRemove) throw 'card-11 btn data-remove-item not found'
+        const btnRemove = card.querySelector('[data-removefromcart-item_btn]')
+        if (!btnRemove) throw 'data-removefromcart-item_btn not found'
 
         const counter = card.querySelector('.card-12')
         if (!counter) throw 'card-12 counter not found'
-        const btnDecrement = counter.querySelector('[data-decrement]')
-        if (!btnDecrement) throw 'card-12 counter btn-decrement not found'
-        const btnIncrement = counter.querySelector('[data-increment]')
-        if (!btnIncrement) throw 'card-12 counter btn-increment not found'
+        const btnDecrement = counter.querySelector('[data-decrement_btn]')
+        if (!btnDecrement) throw 'card-12 counter data-decrement_btn not found'
+        const btnIncrement = counter.querySelector('[data-increment_btn]')
+        if (!btnIncrement) throw 'card-12 counter data-increment_btn not found'
         const input = counter.querySelector('input')
         if (!input) throw 'card-12 counter input not found'
 
@@ -242,15 +240,108 @@ const initCard11 = (cards) => {
     })
 }
 
+const fireCreateServiceDialog = (dialogId) => {
+    const dialog = document.querySelector(`#${dialogId}`)
+    if (!dialog) throw `${dialogId} not found`
+
+    const formStep1 = dialog.querySelector('#create-service-step-1_form')
+    if (!formStep1) throw 'create-service-step-1_form not found'
+
+    handleDialog(dialogId, 'add')
+    formStep1.classList.add('active')
+}
+
+const configCreateServiceDialog = (dialogId) => {
+    const dialog = document.querySelector(`#${dialogId}`)
+    if (!dialog) throw `${dialogId} not found`
+
+    const formStep1 = dialog.querySelector('#create-service-step-1_form')
+    if (!formStep1) throw 'create-service-step-1_form not found'
+    const formSelectCustomerStep2 = dialog.querySelector('#create-service-select-customer-step-2_form')
+    if (!formSelectCustomerStep2) throw 'create-service-select-customer-step-2_form not found'
+    const formDeviceDetailStep3 = dialog.querySelector('#create-service-device-detail-step-3_form')
+    if (!formDeviceDetailStep3) throw 'create-service-device-detail-step-3_form not found'
+    const formDeviceFailureStep4 = dialog.querySelector('#create-service-device-failure-step-4_form')
+    if (!formDeviceFailureStep4) throw 'create-service-device-failure-step-4_form not found'
+    const formStep5 = dialog.querySelector('#create-service-confirmation-step-5_form')
+    if (!formStep5) throw 'create-service-confirmation-step-5_form not found'
+
+    const btnSearchCustomer = dialog.querySelector('[data-search-customer]')
+    if (!btnSearchCustomer) throw 'btn data-search-customer nor found'
+
+    btnSearchCustomer.onclick = () => {
+        formStep1.classList.remove('active')
+        formSelectCustomerStep2.classList.add('active')
+    }
+
+    formSelectCustomerStep2.onsubmit = (e) => {
+        e.preventDefault()
+
+        // TODO: select-customer for service logic
+
+        formSelectCustomerStep2.classList.remove('active')
+        formDeviceDetailStep3.classList.add('active')
+    }
+
+    formDeviceDetailStep3.onsubmit = (e) => {
+        e.preventDefault()
+
+        // TODO: device-detail for service logic
+
+        formDeviceDetailStep3.classList.remove('active')
+        formDeviceFailureStep4.classList.add('active')
+    }
+
+    formDeviceFailureStep4.onsubmit = (e) => {
+        e.preventDefault()
+
+        // TODO: device-failure for service logic
+
+        formDeviceFailureStep4.classList.remove('active')
+        formStep5.classList.add('active')
+    }
+
+    formStep5.onsubmit = (e) => {
+        e.preventDefault()
+
+        formStep5.classList.remove('active')
+        handleDialog(dialogId, 'remove')
+    }
+}
+
+const initSelect = (cards) => {
+    cards.forEach((card) => {
+        const btnSelect = card.querySelector('[data-select_btn]')
+        if (!btnSelect) throw 'data-select_btn not found'
+        const btnUnselect = card.querySelector('[data-unselect_btn]')
+        if (!btnUnselect) throw 'data-unselect_btn not found'
+
+        btnSelect.onclick = () => {
+            btnUnselect.classList.add('active')
+            btnSelect.classList.remove('active')
+
+            // TODO: customer-select logic
+        }
+
+        btnUnselect.onclick = () => {
+            btnSelect.classList.add('active')
+            btnUnselect.classList.remove('active')
+
+            // TODO: customer-unselect logic
+        }
+    })
+}
+
 const render = () => {
     initDialog()
     initTable()
     initCard8()
-    initCard11([...document.querySelectorAll('.card-11')])
-    initFieldsetList()
+    
+    initCart([...document.querySelectorAll('.card-11[data-cart]')])
+    initSelect([...document.querySelectorAll('.card-11[data-select]')])
 
     registerOrder()
-    initNewSaleDialog('new-sale_dialog', [])
+    configCreateSaleDialog('create-sale_dialog', [])
 }
 
 render()
